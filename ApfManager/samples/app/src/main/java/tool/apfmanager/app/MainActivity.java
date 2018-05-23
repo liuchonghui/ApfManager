c29d36f65e82d2f21110898fe02eba4c7b573d5b
liuchonghui <liuchonghui@xiaomi.com>
2018-05-23 15:53:14 +0800

    完成测试app布局和基础功能
    N/A
    
    Signed-off-by: liuchonghui <liuchonghui@xiaomi.com>

--- ApfManager/samples/app/src/main/java/tool/apfmanager/app/MainActivity.java
+++ ApfManager/samples/app/src/main/java/tool/apfmanager/app/MainActivity.java
@@ -1,14 +1,23 @@
 package tool.apfmanager.app;
 
 import android.app.Activity;
+import android.compact.utils.FileCompactUtil;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.kuaiest.video.cpplugin.PluginUtil;
+import com.limpoxe.fairy.content.PluginDescriptor;
+import com.limpoxe.fairy.manager.PluginManagerHelper;
+
+import java.io.File;
 
 public class MainActivity extends Activity {
 
@@ -16,21 +25,69 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
 
-        final Button btn1 = (Button) findViewById(R.id.btn1);
-        final TextView btn1ret = (TextView) findViewById(R.id.btn1_ret);
-        final Button btn2 = (Button) findViewById(R.id.btn2);
-        final TextView btn2ret = (TextView) findViewById(R.id.btn2_ret);
+    protected void initData() {
+        PluginUtil.Companion.checkAndInstallPlugins(getApplicationContext(), false);
+    }
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+    protected void initView() {
+        final CheckBox cp_plugin_switcher = (CheckBox) findViewById(R.id.cp_plugin_switcher);
+        final ViewGroup cp_plugin_panel = (ViewGroup) findViewById(R.id.cp_plugin_panel);
+        cp_plugin_switcher.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(final View view) {
-            }
-        });
+            public void onCheckedChanged(CompoundButton buttonView, boolean checked) {
+                if (checked) {
+                    for (PluginDescriptor pd : PluginManagerHelper.getPlugins()) {
+                        String pluginPackageName = pd.getPackageName();
+                        boolean pluginEnable = pd.isEnabled();
+                        boolean pluginStandAlone = pd.isStandalone();
+                        String pluginVersion = pd.getVersion();
+                        String pluginPath = pd.getInstalledPath();
+                        String pluginMd5 = FileCompactUtil.getMD5(new File(pluginPath));
+                        String desc = pluginPackageName + "|version|" + pluginVersion + "|enable|" + pluginEnable + "|standalone|" + pluginStandAlone + "|path|" + pluginPath + "|md5|" + pluginMd5;
+                        final TextView text = new TextView(cp_plugin_switcher.getContext());
+                        text.setText(desc);
+                        text.setTag(pluginPackageName);
+                        text.setOnLongClickListener(new View.OnLongClickListener() {
+                            @Override
+                            public boolean onLongClick(View v) {
+                                String tag = (String) v.getTag();
+                                int code = PluginManagerHelper.remove(tag);
+                                Toast.makeText(v.getContext(), PluginUtil.Companion.getPluginErrMsg(v.getContext().getApplicationContext(), code), Toast.LENGTH_LONG).show();
+                                cp_plugin_panel.removeView((View) text.getParent());
+                                cp_plugin_panel.invalidate();
+                                return true;
+                            }
+                        });
 
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View view) {
+                        final Button btn = new Button(cp_plugin_switcher.getContext());
+                        btn.setText("测试");
+                        btn.setTag(pluginVersion.substring(0, pluginVersion.indexOf("_")));
+                        btn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                String tag = (String) v.getTag();
+                                Toast.makeText(v.getContext(), tag, Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+                        LinearLayout cont = new LinearLayout(cp_plugin_switcher.getContext());
+                        cont.setOrientation(LinearLayout.HORIZONTAL);
+                        cont.setGravity(Gravity.CENTER_VERTICAL);
+                        LinearLayout.LayoutParams textPl = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
+                        cont.addView(text, textPl);
+                        LinearLayout.LayoutParams btnPl = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 0.33f);
+                        cont.addView(btn, btnPl);
+                        cp_plugin_panel.addView(cont);
+                    }
+                    cp_plugin_panel.invalidate();
+                } else {
+                    cp_plugin_panel.removeAllViews();
+                    cp_plugin_panel.invalidate();
+                }
             }
         });
     }
--- ApfManager/samples/app/src/main/java/tool/apfmanager/app/MainApplication.java
+++ ApfManager/samples/app/src/main/java/tool/apfmanager/app/MainApplication.java
@@ -3,15 +3,26 @@ package tool.apfmanager.app;
 import android.app.Application;
 import android.content.Context;
 
+import com.kuaiest.video.cpplugin.PluginUtil;
+
 public class MainApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        if (PluginUtil.Companion.isPluginProcess(this)) {
+            return;
+        }
     }
 
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
+        PluginUtil.Companion.applicationAttachBaseContext(this, base);
+    }
+
+    @Override
+    public Context getBaseContext() {
+        return PluginUtil.Companion.applicationGetBaseContext(this, super.getBaseContext());
     }
 }
